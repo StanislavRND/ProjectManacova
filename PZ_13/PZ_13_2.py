47be0afc468782f29c3bd29e56ba037600e1c6e7
StanislavRND <stassinelnikov6@gmail.com>
2024-03-13 12:30:48 +0300

    Editing PZ_13

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -2,24 +2,17 @@
 # 2 раза.
 import random
 
-def modify_matrix(matrix):
-    n = len(matrix)
-    for i in range(n):
-        for j in range(n):
-            if i != j:
-                matrix[i][j] *= 2
-    return matrix
-
 rows = 3
 cols = 3
 
 matrix = [[random.randint(1, 20) for _ in range(cols)] for _ in range(rows)]
 
+modified_matrix = [[el * 2 if i != j else el for i, el in enumerate(row)] for j, row in enumerate(matrix)]
+
 print('Исходная матрица: ')
 for row in matrix:
     print(row)
 
-modified_matrix = modify_matrix(matrix)
 
 print('Модифицированная матрица: ')
 for row in modified_matrix:
